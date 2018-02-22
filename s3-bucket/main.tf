ecfc321981a56e737141b2a11017449c0a77f4e5
Michael Warkentin <mwarkentin@gmail.com>
2018-02-21 22:44:04 -0500

    Remove s3 encryption

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -11,13 +11,4 @@ resource "aws_s3_bucket" "bucket" {
     target_bucket = "${var.logging_target_bucket}"
     target_prefix = "aws/s3/${var.name}/"
   }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "${var.kms_master_key_id}"
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
 }
